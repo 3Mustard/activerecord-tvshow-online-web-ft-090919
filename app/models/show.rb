bbec5eabc5d8a6525e7a05096eb684961af2e4be
Matthew C <m23.cullen@gmail.com>
2019-10-18 17:18:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,31 @@
 class Show < ActiveRecord::Base 
   
-  def high
+  def self.highest_rating
+    
+  end 
+  
+  def self.most_popular_show
+    
+  end 
+  
+  def self.lowest_rating
+    
+  end 
+  
+  def self.least_popular_show
+    
+  end 
+  
+  def self.ratings_sum
+    
+  end 
+  
+  def self.popular_shows
+    
+  end 
+  
+  def shows_by_alphabetical_order
+    
+  end 
+  
 end 
