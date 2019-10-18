dbe6f28c7e866f781fd28eae5a466b0761482aeb
Matthew C <m23.cullen@gmail.com>
2019-10-18 17:22:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,15 +5,15 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-    self.where("rating")
+    self.where("rating = ?", self.highest_rating).first
   end 
   
   def self.lowest_rating
-    
+    self.minimun(:rating)
   end 
   
   def self.least_popular_show
-    
+    self.where("rating = ?", self.lowest_rating).first
   end 
   
   def self.ratings_sum
